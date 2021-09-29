0080ad7dc8abfb772ca4c4221e39cfbeead2c609
Kishan Nirghin <kishan@itslogical.nl>
2021-09-29 11:52:59 +0200

    Add working example for code mirroring

--- src/components/server/index.tsx
+++ src/components/server/index.tsx
@@ -1,25 +1,36 @@
-import React, {useState } from 'react'
+import React, {useEffect, useState } from 'react'
 import style from './server.module.css';
 import { subscribe } from '../../gameRepository';
 
 export default function Server() {
 
-    const [game, setGame] = useState({});
+    const [game, setGame] = useState<any>({});
+    const [code, setCode] = useState<string[]>([]);
 
-    function createGame() {
-        subscribe("test", (data: Object) => {
+
+    useEffect(( ) => {
+        subscribe("test", (data: any) => {
             console.log({data});
             setGame(data);
-        })
-    }
+
+            let tempCode = [];
+            for (const code in data){
+                tempCode.push(data[code]);
+            }
+            setCode(tempCode);
+        });
+    }, [])
+
+
     return (
         <div className={style.container}>
             <h1 >Let this be a server</h1>
             <h2>Status: idle</h2>
 
-            <button onClick={(ev) => createGame()}>Create game</button>
+            { code.map(c => 
+                <textarea value={c}></textarea>
+            )};
 
-            
         </div>
     )
 }
